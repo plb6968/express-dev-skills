7bf63ca11f8ab19689f3b5f4d153a78f29b832c5
Phillip Black <phil@Phillips-MacBook-Air.local>
2022-10-18 20:21:50 -0500

    add link to home and link to skills page.

---
+++ models/skills.js
@@ -0,0 +1,6 @@
+const skills = [
+    {skill: 'CSS', aquired: True},
+    {skill: 'HTML', aquired: True},
+    {skill: 'javaScript', aquired: True},
+    {skill: 'Express', aquired: False}
+];
--- routes/skills.js
+++ routes/skills.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('skills', { title: 'My Dev Skills'});
 });
 
 module.exports = router;
